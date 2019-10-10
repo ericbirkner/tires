0acbd814ef92a6d9820680b4aac372737ea9d730
Eric Birkner <ericbirkner@gmail.com>
2019-10-10 16:20:14 -0300

    ajustes

--- src/components/Historia.vue
+++ src/components/Historia.vue
@@ -13,11 +13,11 @@
           <swiper-slide v-for="historia in historias" v-bind:key="historia.id">
             <div class="row">
 
-              <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 thumb-slide web">
+              <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 thumb-slide web">
                 <img :src="historia.imagen" class="img-fluid"/>
               </div>
 
-              <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                 <div class="datos-historia">
                   <h2>
                     {{historia.ano}}
